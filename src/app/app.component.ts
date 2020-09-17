10a592639329c9c92e6fca53488ff4bf517e8e0d
Bircan YÜRÜKE <bircanyuruk@gmail.com>
2020-09-17 16:19:34 +0300

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'udemy-angular-course';
+  constructor(private toastr: ToastrService, 
+              private spinner: NgxSpinnerService)
+   {}
+
+  ngOnInit() {
+    this.showsucces();
+    this.showSpinner();
+  }
+
+  showsucces() {
+    this.toastr.success("Udemy angular 9 projemize hoş geldiniz.",
+     "Giriş Başarılı");
+  }
+
+  showSpinner(){
+    this.spinner.show();
+ 
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 2000);
+  }
+
 }
