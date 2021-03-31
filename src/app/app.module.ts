6cd46bc4074271b686d129fbe7e32e4efe19352d
bircanyuruk <bircanyuruk@gmail.com>
2021-03-31 17:39:27 +0300

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from "ngx-spinner";
 
 @NgModule({
   declarations: [
@@ -10,9 +16,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule, // required animations module
+    // ToastrModule.forRoot() // ToastrModule added    
+      ToastrModule.forRoot({
+        timeOut: 4000,
+        progressBar: true,
+        easing: "ease-in",
+        closeButton: true,
+        progressAnimation: "decreasing",
+        preventDuplicates: true,
+        positionClass: "toast-bottom-left"
+      }), // ToastrModule added    
+      NgxSpinnerModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
